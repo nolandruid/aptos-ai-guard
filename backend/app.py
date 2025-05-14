ad4a5712317a414a1f276f366e9ae245f5d37cb9
jessica-slee4275 <jessica.slee4275@gmail.com>
2025-05-14 13:40:35 -0400

    Update risk score call from GET to POST

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/risk_score', methods=['GET'])
+@app.route('/risk_score', methods=['POST'])
 def risk_score():
+    data = request.get_json()
+    recipient = data.get('recipient')
+
     dummy_response = {
+        "recipient": recipient,
         "label": "Trusted",
         "confidence": 0.95
     }
